2cf536b61b6524bfb90903d2d1937c863894409a
charan51 <cvcharan51@gmail.com>
2018-12-24 17:30:05 +0530

    added assignment

---
+++ app.js
@@ -0,0 +1,46 @@
+const studentDetails = {
+    "studentDetails": [
+        {
+           "name": "Charan",
+           "age": 22,
+           "weight": 55,
+           "height": 173,
+           "friends": [
+               "Kiran", "Ram", "Ali", "Kumar", "Tom"
+           ],
+           "College": {
+               "name": "Bangalore College",
+               "city": "Bangalore",
+               "pincode": 560044
+           } 
+        },
+        {
+            "name": "Raj",
+            "age": 33,
+            "weight": 52,
+            "height": 155,
+            "friends": [
+                "Rakul", "Salman", "Ali", "Kumar", "Tom"
+            ],
+            "College": {
+                "name": "City College",
+                "city": "Delhi",
+                "pincode": 560034
+            } 
+         },
+         {
+            "name": "Tom",
+            "age": 32,
+            "weight": 66,
+            "height": 133,
+            "friends": [
+                "Jam", "Sam", "Ali", "Kumar", "Tom"
+            ],
+            "College": {
+                "name": "Christ College",
+                "city": "Chennai",
+                "pincode": 560022
+            } 
+         }
+    ]
+};
